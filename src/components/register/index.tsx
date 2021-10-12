ccee4f523d65d7c1a7e4c9d7a5c529f58a0f21c2
Tommy Knight <78544868+Tommy-Knight@users.noreply.github.com>
2021-10-12 12:39:47 +0100

    🌋

--- src/components/register/index.tsx
+++ src/components/register/index.tsx
@@ -21,14 +21,17 @@ const Register = (props: Props) => {
 	const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
 		try {
-			const resp = await fetch(`http://localhost:3069/auth/register`, {
-				method: 'POST',
-				credentials: 'include',
-				headers: {
-					'Content-Type': 'application/json',
-				},
-				body: bodyJSON,
-			});
+			const resp = await fetch(
+				`https://photide-server.herokuapp.com/auth/register`,
+				{
+					method: 'POST',
+					credentials: 'include',
+					headers: {
+						'Content-Type': 'application/json',
+					},
+					body: bodyJSON,
+				}
+			);
 			const data = await resp.json();
 			dispatch(userAction(data));
 			if (resp.ok) history.push('/');
@@ -45,14 +48,17 @@ const Register = (props: Props) => {
 	const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
 		try {
-			const resp = await fetch(`http://localhost:3069/auth/login`, {
-				method: 'POST',
-				credentials: 'include',
-				headers: {
-					'Content-Type': 'application/json',
-				},
-				body: bodyJSONLogin,
-			});
+			const resp = await fetch(
+				`https://photide-server.herokuapp.com/auth/login`,
+				{
+					method: 'POST',
+					credentials: 'include',
+					headers: {
+						'Content-Type': 'application/json',
+					},
+					body: bodyJSONLogin,
+				}
+			);
 			const data = await resp.json();
 			if (resp.ok) {
 				dispatch(userAction(data));
